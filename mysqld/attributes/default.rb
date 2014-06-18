42a2e7c624b49e13da9e7a6c4b65639f959fdfa0
TORU KOKUBUN <kokubun@d-s-b.jp>
2014-06-18 15:13:47 +0900

    edit mysqld attribultes.

--- mysqld/attributes/default.rb
+++ mysqld/attributes/default.rb
@@ -1,4 +1,4 @@
-default['mysqld']['cnf'][:root_pwd]       = "vagrant"
+default['mysqld'][:root_pwd]              = "vagrant"
 default['mysqld']['cnf'][:charset]        = "utf8"
 default['mysqld']['cnf'][:storage_engine] = "InnoDB"
 
